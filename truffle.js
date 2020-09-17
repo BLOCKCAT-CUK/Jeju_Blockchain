4913e8a49a22f91959148b6f757f3619cd4f4fcb
InChul <49394875+inchori@users.noreply.github.com>
2020-09-17 21:32:36 +0900

    hide private key

--- truffle.js
+++ truffle.js
@@ -1,6 +1,6 @@
 const HDWalletProvider = require("truffle-hdwallet-provider-klaytn");
 const URL = 'https://api.baobab.klaytn.net:8651';
-const PRIVATE_KEY = '0xc71a9a2eadd3583106ebe3b96f7de8891834ef5cea44b0b80d0603381fae523a';
+const PRIVATE_KEY = '';
 const NETWORK_ID = '1001'
 const GASLIMIT = '3000000'
 
